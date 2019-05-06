6a740af1099d1bc45621ac8a11bc2a8303aec0a9
jacek_debek <jacek141198@gmail.com>
2019-05-07 01:19:54 +0200

    Updates

---
+++ precache-manifest.76e7881e13a11da16845b77648a5fd89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f1cc89656827904c089130c5d60274b",
+    "url": "/Diagramming_App/index.html"
+  },
+  {
+    "revision": "a3664fadae3d8baa8800",
+    "url": "/Diagramming_App/static/css/main.cdb4acfe.chunk.css"
+  },
+  {
+    "revision": "9003131602e5733a0d65",
+    "url": "/Diagramming_App/static/js/2.fcb12772.chunk.js"
+  },
+  {
+    "revision": "a3664fadae3d8baa8800",
+    "url": "/Diagramming_App/static/js/main.1f6968bd.chunk.js"
+  },
+  {
+    "revision": "0e895c53d2dab21d1893",
+    "url": "/Diagramming_App/static/js/runtime~main.49305017.js"
+  }
+]);
